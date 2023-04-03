4b969d3a1c137bf5a0c7fd14f2a3c98c89d278dc
alekseihelik <ahelik4984@bths.edu>
2023-04-03 08:22:21 -0400

    updated design document

--- src/LogicalVolumes.java
+++ src/LogicalVolumes.java
@@ -1,24 +1,24 @@
 public class LogicalVolumes extends Data{
 
     private int size;
-    private String group;
+    private String volumeGroup;
 
     public LogicalVolumes(String name, String size, String group){
         super(name);
         this.size = Integer.parseInt(size.substring(0, size.length()));
-        this.group = group;
+        volumeGroup = group;
     }
 
     public int getSize() {
         return size;
     }
 
-    public String getGroup() {
-        return group;
+    public String getVolumeGroup() {
+        return volumeGroup;
     }
 
     public String toString()
     {
-        return super.getName() + ": [" + size  + "] [" + group + "] [" + super.getUuid().toString() + "]";
+        return super.getName() + ": [" + size  + "] [" + volumeGroup + "] [" + super.getUuid().toString() + "]";
     }
 }
